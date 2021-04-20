274e92769cc9418316a8ae2aab41485e89c818bb
muhammadnasif <1705109@ugrad.cse.buet.ac.bd>
2021-04-21 04:15:13 +0600

    eof fixed and scope fixed

--- CSE_310_Compiler/Symbol Table/symbolTable.cpp
+++ CSE_310_Compiler/Symbol Table/symbolTable.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
- 
+
 using namespace std;
- 
+
+ofstream outLog("1705109_log.txt");
+
 class SymbolInfo
 {
     string name;
     string type;
     ///POINTER OF THIS CLASS TO RESOLVE COLLISION DURING HASHING
- 
+
 public:
     void setName(string x)            ///SETTER
     {
@@ -25,9 +27,9 @@ public:
     {
         return type;
     }
- 
+
 };
- 
+
 class ScopeTable
 {
     ScopeTable *parentScope;
@@ -82,22 +84,29 @@ public:
     }
     void printScope()
     {
-        cout<<"ScopeTable #"<<address<<"\n";
+        outLog<<"ScopeTable #"<<address<<"\n";
         for(int i=0; i<bucket_size; i++)
         {
-            cout<<i<<"-->";
-            /*for(auto it=bucket[i].begin(); it!=bucket[i].end(); it++)
-            {
-                cout<<" <"<<it->getName()<<" : "<<it->getType()<<">";
-            }*/
-            for(int j=0; j<bucket[i].size(); j++)
-            {
-                cout<<" <" << bucket[i][j].getName() << " : " << bucket[i][j].getType() << ">" ;
+            if(!bucket[i].empty()){
+                //outLog<<i<<"-->";
+                outLog<<i<<"-->";
+
+                /*for(auto it=bucket[i].begin(); it!=bucket[i].end(); it++)
+                {
+                    outLog<<" <"<<it->getName()<<" : "<<it->getType()<<">";
+                }*/
+                for(int j=0; j<bucket[i].size(); j++)
+                {
+                    //outLog<<" <" << bucket[i][j].getName() << " : " << bucket[i][j].getType() << ">" ;
+                    outLog<<" <" << bucket[i][j].getName() << " : " << bucket[i][j].getType() << ">" ;
+                }
+                ///outLog<<"\n";
+                outLog<<"\n";
             }
-            cout<<"\n";
+
         }
     }
- 
+
     SymbolInfo* LookUp(string val)
     {
         int idx = Hash_Function(val,bucket_size);
@@ -106,14 +115,15 @@ public:
             if( bucket[idx][i].getName().compare(val) == 0 )
             {
                 pos_found = i;
-                cout<<"Found in ScopeTable# "<<address<<" "<<" at position "<<idx<<", "<<pos_found<<endl;
+                //outLog<<"Found in ScopeTable# "<<address<<" "<<" at position "<<idx<<", "<<pos_found<<endl;
+                outLog<<"Found in ScopeTable# "<<address<<" "<<" at position "<<idx<<", "<<pos_found<<endl;
                 return &bucket[idx][i];
             }
         }
- 
+
         return NULL;
     }
- 
+
     bool deleteSymbol(string sign)
     {
         int idx = Hash_Function(sign,bucket_size);
@@ -124,15 +134,17 @@ public:
             {
                 pos_found=counter;
                 bucket[idx].erase(it);
-                cout<<"Deleted Entry "<<idx<<", "<<counter<<" from current ScopeTable\n";
+                //outLog<<"Deleted Entry "<<idx<<", "<<counter<<" from current ScopeTable\n";
+                outLog<<"Deleted Entry "<<idx<<", "<<counter<<" from current ScopeTable\n";
+
                 return true;
             }
             counter++;
         }
         return false;
     }
- 
- 
+
+
     string getScopeAddress()
     {
         string str = address;
@@ -140,7 +152,15 @@ public:
     }
     void setScopeAddress(string parent_address,int x)   ///x / tot_child
     {
-        address = parent_address + "." + to_string(x);
+        string str = parent_address;
+        int q = x;
+        if(parent_address.compare("")==0){
+            address="1";
+        }
+        else{
+            address = parent_address + "." + to_string(x);
+        }
+
     }
     void incChild()
     {
@@ -156,24 +176,25 @@ public:
         string str = to_string(hash_index)+", "+to_string(len);
         return str;
     }
- 
+
 };
- 
- 
- 
+
+
+
 class SymbolTable
 {
     ScopeTable *currentScope=nullptr;
- 
+
     int bucketSize, hash_index;
     vector<ScopeTable> scopes;
 public:
     SymbolTable(int x)
     {
+
         bucketSize = x;
         currentScope = new ScopeTable(x);
     }
- 
+
     ~SymbolTable(){
         delete currentScope;
     }
@@ -181,19 +202,19 @@ public:
     {
         currentScope->printScope();
     }
- 
+
     void printAllScope()
     {
-    /*
-        currentScope->printScope();
-        int range = scopes.size();
-        ScopeTable tempScope(bucketSize);
-        for(int i=range-1; i>=0; i--)
-        {
-            tempScope = scopes[i];
-            tempScope.printScope();
-        }
-    */
+        /*
+            currentScope->printScope();
+            int range = scopes.size();
+            ScopeTable tempScope(bucketSize);
+            for(int i=range-1; i>=0; i--)
+            {
+                tempScope = scopes[i];
+                tempScope.printScope();
+            }
+        */
         ScopeTable* tempScope = currentScope;
         while(tempScope != nullptr){
             tempScope->printScope();
@@ -204,50 +225,54 @@ public:
     {
         if(currentScope->Insert(x,y))
         {
-            cout<<"Inserted in ScopeTable# "<<currentScope->getScopeAddress()<<" at position "<<currentScope->getHashIndex()<<"\n";
+            //outLog<<"Inserted in ScopeTable# "<<currentScope->getScopeAddress()<<" at position "<<currentScope->getHashIndex()<<"\n";
+            //outLog<<"Inserted in ScopeTable# "<<currentScope->getScopeAddress()<<" at position "<<currentScope->getHashIndex()<<"\n";
             return true;
         }
         else
         {
             return false;
         }
- 
+
     }
     void EnterScope()
     {
- 
-        /*
+
+        ScopeTable* current = new ScopeTable(bucketSize);
         currentScope->incChild();
-        scopes.push_back(*currentScope);
-        currentScope = new ScopeTable(bucketSize);
-        currentScope->setScopeAddress(parentScope->getScopeAddress(),parentScope->getChild());
-        cout<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" created\n";
-        */
+        int val = currentScope->getChild();
         if(this->currentScope == nullptr){
-            this->currentScope = currentScope;
+            this->currentScope = current;
             currentScope->setScopeAddress(currentScope->getScopeAddress(),currentScope->getChild());
+
             currentScope->setparentScope(nullptr);
         }
         else{
-            currentScope->setScopeAddress(currentScope->getScopeAddress(),currentScope->getChild());
-            cout<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" created\n";
- 
+            current->setScopeAddress(currentScope->getScopeAddress(),currentScope->getChild());
+            current->setparentScope(currentScope);
+            this->currentScope=current;
+            //outLog<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" created\n";
+            //outLog<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" created\n";
+
         }
     }
     void ExitScope()
     {
-        cout<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" removed\n";
+        int x = currentScope->getChild();
+        ///outLog<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" removed\n";
+        //outLog<<"ScopeTable with id "<<currentScope->getScopeAddress()<<" removed\n";
         /*
         delete currentScope;
         currentScope = new ScopeTable(bucketSize);
         currentScope = &scopes.back();
         scopes.pop_back();
         */
+
         if(currentScope->getparentScope()!=nullptr){
             currentScope = currentScope->getparentScope();
         }
     }
- 
+
     bool removeSymbol(string sign)
     {
         SymbolInfo *temp = currentScope->LookUp(sign);
@@ -258,20 +283,21 @@ public:
         }
         else
         {
-                cout<<"Not found\n";
-            cout<<sign<<" not found\n"<<endl;
+            outLog<<"Not found\n";
+            outLog<<sign<<" not found\n"<<endl;
+
             return false;
         }
     }
- 
+
     SymbolInfo* LookUp(string name)
     {
         SymbolInfo *temp =  currentScope->LookUp(name);
- 
+
         if( temp == NULL )
         {
             int range = scopes.size();
- 
+
             for(int i=range-1; i>=0; i--)
             {
                 temp = scopes[i].LookUp(name);
@@ -280,10 +306,10 @@ public:
                     return temp;
                 }
             }
-            cout<<"Not Found"<<endl;
+            outLog<<"Not Found"<<endl;
         }
         else{
-                return temp;
+            return temp;
         }
     }
     string getAddress()
@@ -294,36 +320,38 @@ public:
     {
         return "";  ///HAVE TO WORK HERE
     }
- 
+
 };
- 
- 
+
+
 int main()
 {
- 
+
     ifstream in("input.txt");
     int len=0;
-    //cin>>len;
     in>>len;
     SymbolTable S(len);
     string command, x,y;
-    while(!in.eof())
+    while(1)
     {
-        //cin>>command;
         in>>command;
+        if(in.eof()){
+            break;
+        }
+        //cin>>command;
         cout<<command;
         if(command.compare("I")==0)            ///INSERT
         {
             //cin>>x>>y;
             in>>x>>y;
-            cout<<x<<""<<y<<endl;
+
             if(S.Insert(x,y))
             {
- 
+
             }
             else
             {
-                cout<<"Already exists in current scope table\n";
+                outLog<<"Already exists in current scope table\n";
             }
         }
         else if(command.compare("L")==0)        ///LOOKUP
@@ -340,28 +368,27 @@ int main()
         }
         else if(command.compare("P")==0)        ///PRINT
         {
-            //cin>>x;
             in>>x;
-            cout<<x<<endl;
             if(x.compare("A")==0)
             {
                 S.printAllScope();
             }
             else if(x.compare("C")==0)
             {
- 
                 S.printCurrentScope();
             }
         }
         else if(command.compare("S")==0)        ///ENTER SCOPE
         {
             S.EnterScope();
+            outLog<<"ScopeAddress: "<<S.getAddress()<<endl;
         }
         else if(command.compare("E")==0)        ///EXIT SCOPE
         {
- 
             S.ExitScope();
+
         }
     }
     return 0;
 }
+
